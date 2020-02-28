e40258b7190d40bbbfab44ee6e726ba106f88341
DaiJitao <976185561@qq.com>
2020-02-28 10:08:33 +0800

    爬虫关键字

--- web_spider/demo/selenium_chrome_proxy.py
+++ web_spider/demo/selenium_chrome_proxy.py
@@ -3,6 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
+
+'''
+参考网址：
+https://blog.csdn.net/qq_32502511/article/details/101536325
+'''
 proxy_file = r"E:\data\browsermob-proxy-2.1.4\bin\browsermob-proxy.bat"
 try:
     server = Server(proxy_file)
@@ -13,7 +18,10 @@ try:
 
     chrome_options.add_argument("--proxy-server={0}".format(proxy.proxy))
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36")
-    chrome_options.add_argument("authority=www.iesdouyin.com")
+    chrome_options.add_argument("authority='www.iesdouyin.com'")
+    accept_language = "zh-CN,zh;q=0.9,en;q=0.8";
+    chrome_options.add_argument("accept-language="+accept_language)
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-automation']) # 发爬虫关键字
 
 
     driver = webdriver.Chrome(chrome_options=chrome_options)
