d136a7f2009eebc8288f364813b231d046ddce92
hejr <hejinrong@news.cn>
2020-04-30 16:00:36 +0800

    add

--- NLP/cut_file.py
+++ NLP/cut_file.py
@@ -7,11 +7,12 @@ def clean_text(text):
     # 去掉url地址
     text = re.sub('''http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+''', '', text)
     # 去掉@某某某  text = re.sub(r"@.{1,12}\s", "，", text)
-    text = re.sub(r"\[.{0,5}\]|//|@.{1,15}[:\s]{1,2}", "，", text)
-    text = re.sub("[(。，)(！，)(？，)(、，)@(//@)～>\(\)]{1,}", "，", text)
+    text = re.sub(r"//@[^@]{1,20}:\s{0,1}|\[.{1,5}\]|@[^@]{1,}:\s{0,1}|@.{1,10}\s", "，", text)
+    # text = re.sub(r"\[.{0,5}\]|//@.{1,15}[:\s]{1,2}|@.{1,15}[:\s]{1,2}", "，", text)
+    text = re.sub(r"分享来自：.{1,6}\s", "", text)
+    text = re.sub(r"(？，){1,3}|(。，){1,3}|(！，){1,6}","。", text)
     # 多个问号变为一个问号
-    text = re.sub(r"[。？\?]{2,}", "？", text)
-    text = re.sub(r"[！!]{2,}", "！", text)
+    text = re.sub(r"[！!。？\?]{2,}", "！", text)
     # 去掉#某某某# 和单个字母单个数字
     text = re.sub(r"动态|转发|微博|微博转|转发微博|#[^#]{1,10}#|展开全文|([a-zA-Z]|[0-9])", "，", text)
     # ？? -> ?
@@ -25,8 +26,8 @@ def clean_text(text):
     text = re.sub(not_chinese, " ", text)
     text = re.sub(u".[岁个年只]的", "。", text)
     # 多个空格变为1个
-    text = re.sub(r"[，！。、‘’；：\s]{1,}", " ", text)
-    text = re.sub(r"[(\s，)(\s,),，(（）)(：，)(： ：)(—，)]{1,}", "，", text).strip()
+    text = re.sub(r"[，！。、‘’；：\s]{2,6}", "。", text)
+    # text = re.sub(r"[(\s，),（）： —]{1,}", "。", text).strip()
     text = re.sub(u"[叨哈呵啊操艹]{1,8}[，。？、！‘’；：·]{0,2}", "。", text)
     text = re.sub(r"^[，！。‘’？、]{1}\b", "", text)
     return text
@@ -34,9 +35,9 @@ def clean_text(text):
 
 def clean_cutwords(word):
     if len(word.strip()) == 2:
-        word = re.sub(r"[前上下午中今明后][后年日月天午]|位于|第[一二三四五六七八九十]|[一二三四五六七八九十][上下次条张之只枝件万千个片首种块套双组段座票根口弯湾头对米位篇叶日月克顿吨排层面包圈天]", "", word)
+        word = re.sub(r"[前上下午中今明后][后年日月天午]|位于|第[一二三四五六七八九十]|[这那上下一二三四五六七八九十多][秒上下次条张之只枝件万千个片首种块套双组段座票根口弯湾头对米位篇叶日月克顿吨排层面包圈天年]", "", word)
     if len(word.strip()) == 3:
-        word = re.sub(r"第[一二三四五六七八九十][次条张之只枝件万千个片首种块套双组段座票根口弯湾头对米位篇叶日月克顿吨排层面包圈天]", "", word)
+        word = re.sub(r"第[一二三四五六七八九十][秒次条张之只枝件万千个片首种块套双组段座票根口弯湾头对米位篇叶日月克顿吨排层面包圈天年]", "", word)
 
     return word
 
@@ -54,8 +55,7 @@ if __name__ == '__main__':
     file = "./data/hotword.txt"
     with open(file, mode="r", encoding="utf-8") as fp:
         i = 0
-        for line in fp.readlines()[0:200]:
-            # if i == 8: break
+        for line in fp.readlines()[200:1000]:
             d = json.loads(line)['content']
             print(i )
             print(d)
@@ -66,13 +66,14 @@ if __name__ == '__main__':
             print()
             print(" ".join(c))
             # print("removed Stopwprds:")
-            lst = [word for word in c if word not in stopwords]
-            print(" ".join(lst))
+            lst = [clean_cutwords(word) for word in c if word not in stopwords]
+            print("最终分词："," ".join(lst))
             print("===============================\n\n")
             i += 1
-            break
 
-    text = "今天"
+
+    text = "@古镇珠山发布 "
     print(text)
-    text = clean_cutwords(text)
+    # text = re.sub(r"//@.{1,}:\s{0,1}|\[.{1,5}\]|@.{1,}:\s{0,1}", "，", text)
+    text = clean_text(text)
     print(text)
